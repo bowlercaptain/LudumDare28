3cc4ad6fc84991283dfe1be92211a9aea454fe98
Robert Mock <sompmismyname@gmail.com>
2013-12-13 20:37:30 -0800

    player movement, held objects

---
+++ Assets/holdObjectScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var held:Transform;
+var holdPosition:Vector3;
+
+function Start () {
+
+}
+
+function LateUpdate () {//lateUpdate to prevent strange-looking movement
+	held.position=transform.position+transform.rotation*holdPosition;
+	held.rotation=transform.rotation;
+	//when user uses item, perform "swing" animation, a la minecraft or every other first person game.
+}
