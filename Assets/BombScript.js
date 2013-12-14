686af4ba02486bcce194274f79abec746bf8cc14
Robert Mock <sompmismyname@gmail.com>
2013-12-14 01:57:49 -0800

    Added "Start" button, appropriate game over behavior, shooting bomb ends game

--- Assets/BombScript.js
+++ Assets/BombScript.js
@@ -1,5 +1,7 @@
 ﻿#pragma strict
 
+var mainController:mainControllerScript;//to be able to call GameOver();
+
 //each activatable object (there won't be many) will have an OnKnife, OnRose, OnGun, and OnWatch.
 //Using a tool on an object will SendMessage with one of those functions.
 //The object will animate itself atnd things around accordingly, or send a message with according data to some grand animator.
@@ -23,6 +25,9 @@ function OnRose () {
 
 function OnGun () {
 // explode. idiot.
+
+//Explosion animation! turn on a particle emitter of some kind?
+mainController.GameOver();
 }
 
 function OnWatch () {
--- Assets/mainControllerScript.js
+++ Assets/mainControllerScript.js
@@ -1,17 +1,15 @@
 ﻿#pragma strict
 var clock:float;
 var gameOver:boolean;
-var swingTime:float;
 
 function Start () {
 	clock=0;
-	swingTime=0;
 }
 
 function Update () {
 	clock+=Time.deltaTime;
 	if(clock>60){GameOver();}
-	else if(Input.GetKeyDown("escape")||(gameOver&&(clock>3||Input.GetButtonDown("Fire1")||Input.GetButtonDown("Start")))){//"3" was selected arbitrarily. adjust to sanity.
+	else if(Input.GetKeyDown("escape")||(gameOver&&(clock>3||(Input.GetButtonDown("Fire1")&&clock>.1)||Input.GetButtonDown("Start")))){//"3" was selected arbitrarily. adjust to sanity. Fire1&&clock>.1 so the fire1 event used to trigger the gameOver does not also skip the game over sequence.
 		Application.LoadLevel("main menu");
 	}
 }
@@ -20,4 +18,11 @@ function GameOver () {//can be called by, say, the bomb exploding, as well as th
 	gameOver=true;
 	clock=0;
 	//show "Game Over!"
+}
+
+function OnGUI(){
+	GUI.Label(Rect(20,10,100,100),""+clock);
+	if(gameOver){
+		GUI.Label(Rect(10,10,100,100),"Game over.");//make this bigger, centered, and an image.
+	}
 }
