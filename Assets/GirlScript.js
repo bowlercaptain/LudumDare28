2fd8875b3ec88339dca3b1ea981dd3a411273ea0
Robert Mock <sompmismyname@gmail.com>
2013-12-15 05:02:31 -0800

    initial other characters scripts

---
+++ Assets/GirlScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var mood:String;
+//var aipath:AIPath;
+
+function Start () {
+mood = "Worried about bomb";
+}
+
+function Update () {
+
+}
+
+function OnGun () {
+//You monster.
+
+//create a body?
+Destroy(gameObject);
+}
+
+function OnRose () {
+//wooed, follows Bond to safety.
+mood = "Amorous";
+aipath.speed=78;
+}
