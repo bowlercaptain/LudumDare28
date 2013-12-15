cbfb32d98e3563a8993506deb1378c5055ef75a8
Robert Mock <sompmismyname@gmail.com>
2013-12-15 14:10:16 -0800

    created bomb puzzle

--- Assets/BombScript.js
+++ Assets/BombScript.js
@@ -3,6 +3,8 @@
 var mainController:mainControllerScript;//to be able to call GameOver();
 var clock:float;
 var disarmed:boolean;
+var cylinders:CylinderScript[];
+var selectedIndex:int;
 
 //each activatable object (there won't be many) will have an OnKnife, OnRose, OnGun, and OnWatch.
 //Using a tool on an object will SendMessage with one of those functions.
@@ -10,32 +12,61 @@ var disarmed:boolean;
 
 function Start () {
 clock=0;
+selectedIndex=0;
 disarmed=false;
+	for (var cylinder:CylinderScript in cylinders){
+		cylinder.bomb=this;
+	}
+	//randomize order, thanks to Duck on the unity help forum.
+	for (var i = cylinders.length - 1; i > 0; i--) {
+        var r = Random.Range(0,i);
+        var tmp = cylinders[i];
+        cylinders[i] = cylinders[r];
+        cylinders[r] = tmp;
+        cylinders[i].myIndex = i;
+    }
 }
 
 function Update () {
 clock+=Time.deltaTime;
-if(clock>=60&&!disarmed){
-particleSystem.enableEmission=true;
-mainController.GameOver();
-}
+	if(clock>=60&&!disarmed){
+		particleSystem.Play();
+		mainController.GameOver();
+	}
 }
 
 function OnKnife () {
-//get disarmed
-mainController.bomb=1;
-disarmed=true;
+	mainController.bomb=3;
+	particleSystem.Play();
+	mainController.GameOver();
 }
 
 function OnRose () {
-//confuse everyone around
+	//confuse everyone around
 }
 
 function OnGun () {
-// explode. idiot.
+	// explode. idiot.
 
-//Explosion animation! turn on a particle emitter of some kind?
-mainController.bomb=2;
-particleSystem.Play();
-mainController.GameOver();
+	//Explosion animation! turn on a particle emitter of some kind?
+	mainController.bomb=2;
+	particleSystem.Play();
+	mainController.GameOver();
+}
+
+function OnWatch () {
+	cylinders[selectedIndex].OnWatch();
+}
+
+function StabCylinder (index){
+	if(index==selectedIndex){
+		selectedIndex++;
+		if(selectedIndex>=cylinders.length){
+			disarmed=true;
+			mainController.bomb=1;
+		}
+	} else {
+		//explode!
+		OnKnife();
+	}
 }
---
+++ Assets/CylinderScript.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var bomb:BombScript;
+var buzz:AudioSource;
+var myIndex:int;
+
+function Start () {
+buzz = GetComponent(AudioSource);
+buzz.pitch=Random.value*2.5;
+}
+
+function Update () {
+	buzz.volume*=.95;
+}
+
+function OnGun () {
+	bomb.OnGun();
+}
+
+function OnWatch() {
+	buzz.volume+=.25;
+}
+
+function OnKnife () {
+	bomb.StabCylinder(myIndex);
+}
