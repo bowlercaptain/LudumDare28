4f1da98e3a9059016ce2949c142fa63e9e919c40
marcus.silva <marcus.silva@xirrus.com>
2013-12-14 16:38:15 -0800

    Added Show/Hide Mouse

--- Assets/mainControllerScript.js
+++ Assets/mainControllerScript.js
@@ -4,12 +4,17 @@ var gameOver:boolean;
 
 function Start () {
 	clock=0;
+	Screen.showCursor = false;
 }
 
 function Update () {
 	clock+=Time.deltaTime;
-	if(clock>60){GameOver();}
-	else if(Input.GetKeyDown("escape")||(gameOver&&(clock>3||(Input.GetButtonDown("Fire1")&&clock>.1)||Input.GetButtonDown("Start")))){//"3" was selected arbitrarily. adjust to sanity. Fire1&&clock>.1 so the fire1 event used to trigger the gameOver does not also skip the game over sequence.
+	if(clock>60){
+		GameOver();
+		Screen.showCursor = true;
+	} else if(Input.GetKeyDown("escape")||(gameOver&&(clock>3||(Input.GetButtonDown("Fire1")&&clock>.1)||Input.GetButtonDown("Start")))){
+	//"3" was selected arbitrarily. adjust to sanity. Fire1&&clock>.1 so the fire1 event used to trigger the gameOver does not also skip the game over sequence.
+		Screen.showCursor = true;
 		Application.LoadLevel("main menu");
 	}
 }
